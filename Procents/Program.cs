a3b9c7992dcda9f0ca6d6fbfb2704ba11a12c591
Enotisha <pavlova_pn@skbkontur.ru>
2023-02-26 16:38:21 +0500

    первый коммит, ввод и вывод

---
+++ Procents/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace Procents
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите через пробел три числа: исходную сумму, процентную ставку (в процентах) и срок вклада в месяцах");
+            Console.ReadLine();
+        }
+    }
+}
