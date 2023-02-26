b4506bdddf6e7612e1aa2f4a820c27d78654e927
Enotisha <pavlova_pn@skbkontur.ru>
2023-02-26 17:56:53 +0500

    добавила тело метода

--- Procents/Program.cs
+++ Procents/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Globalization;
 
 namespace Procents
 {
@@ -8,7 +9,15 @@ namespace Procents
         public static void Main(string[] args)
         {
             Console.WriteLine("Введите через пробел три числа: исходную сумму, процентную ставку (в процентах) и срок вклада в месяцах");
-            Console.ReadLine();
+            Console.WriteLine(Calculate(Console.ReadLine()));
+        }
+        public static double Calculate(string userInput)
+        {
+            var contribution = userInput.Split();
+            var deposit = double.Parse(contribution[0], CultureInfo.InvariantCulture);
+            var rate = double.Parse(contribution[1], CultureInfo.InvariantCulture);
+            var period = double.Parse(contribution[2]);
+            return deposit * Math.Pow(1+rate/100/12, period);
         }
     }
 }
